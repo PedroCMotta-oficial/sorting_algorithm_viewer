0827b25dca670aa7fbc18abc5cc5d7132ab292b1
Pedro Costa da Motta <79551989+PedroCMotta-oficial@users.noreply.github.com>
2025-02-12 20:07:45 -0300

    frontend v0.1
    react first contact , aka, learning the process

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700">
+      Simple Component Button
+    </button>
+  );
+}
